dd61d188cad175d70d35daa7f07066ef28a8b8e5
mantaek <hibernal@naver.com>
2024-12-10 04:34:57 +0900

    fade in/out 추가

---
+++ Source/unreal5Study/Cropout/GameInstance/USCropoutGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "USCropoutGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL5STUDY_API UUSCropoutGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
